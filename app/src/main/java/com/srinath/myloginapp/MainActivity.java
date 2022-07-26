a809c9f383da09f9a32dcd1449866d924ec99098
Srinath S <connect2srinath06@gmail.com>
2022-07-26 09:48:16 +0530

    Added : Second Activity

--- app/src/main/java/com/srinath/myloginapp/MainActivity.java
+++ app/src/main/java/com/srinath/myloginapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.srinath.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -25,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL !!!",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
                 }else
                     Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
             }
